2845ee0818fa96935e3b097af93f2b027b5e08f1
mathababhassan <103527892+mathababhassan@users.noreply.github.com>
2024-05-31 19:24:57 +0800

    Task 2 Complete

--- First Game/scripts/coin.gd
+++ First Game/scripts/coin.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 @onready var game_manager = %GameManager
 @onready var animation_player = $AnimationPlayer
+@onready var animation_player2 = $AnimationPlayer
 
 func _on_body_entered(body):
 	game_manager.add_point()
--- First Game/scripts/slime.gd
+++ First Game/scripts/slime.gd
@@ -7,7 +7,7 @@ var direction = 1
 @onready var ray_cast_right = $RayCastRight
 @onready var ray_cast_left = $RayCastLeft
 @onready var animated_sprite = $AnimatedSprite2D
-
+@onready var animated_sprite2 = $AnimatedSprite2D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if ray_cast_right.is_colliding():
